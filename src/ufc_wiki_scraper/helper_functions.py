8f23eb2f8eedff97fb3c8ab2b3f268fb6e4e4294
burakgizlice <burakgizlice114@gmail.com>
2025-01-09 16:40:06 +0100

    implemented get info with regex details on weight, height and reach also included extra functionality which is called ops+info to get opponents and the information of the opponents

--- src/ufc_wiki_scraper/helper_functions.py
+++ src/ufc_wiki_scraper/helper_functions.py
@@ -1,6 +1,7 @@
 """ Helper functions for the scraper """
 from requests import get
 from parsel import Selector
+import re
 
 # Returns the appropriate function for the handler assignment.
 def get_handler(choice):
@@ -8,6 +9,8 @@ def get_handler(choice):
         return get_opponents
     if choice == "info":
         return get_info
+    if choice == "ops+info":
+        return get_ops_with_info
 
 def get_opponents(url):
     response = get(url) # getting the HTML file
@@ -24,18 +27,20 @@ def get_opponents(url):
         record = row.xpath("./td[2]/text()").get()
         method = row.xpath("./td[4]/text()").get()
         date = row.xpath("./td[6]/span/text()").get()
+        if date is None:
+            date = opponents[len(opponents) - 1]["date"]
 
         # obtaining the name, just a tad bit trickier than other fields
         a = row.xpath("./td[3]/a")
         href = a[0].xpath("@href").get() if a else None
         name = a[0].xpath("./text()").get() if a else row.xpath("./td["
                "3]/text()").get()
-        print(name)
 
         # collect data together
         opponent = {
             "name": name.strip('\n'),
-            "url": "https://en.wikipedia.org" + str(href),
+            "url": None if not href else "https://en.wikipedia.org" + str(
+                href),
             "outcome": outcome.strip("\n"),
             "method": method.strip('\n'),
             "date": date,
@@ -62,11 +67,55 @@ def get_info(url):
         "image": "https://en.wikipedia.org" + str(image)
     }
 
-    for row in table_rows[2:12]:
-        th = row.xpath("./th/text()").get().lower()
-        td = row.xpath("./td//text()").getall()
-        info[th] = ''.join(td).strip().replace('\u00a0', ' ').replace(
-            '\u2013', '-')
+    for row in table_rows[2:]:
+        key : str = row.xpath("./th/text()").get()
+        value = row.xpath("./td/text()").get()
 
-    # collecting the data together
+        if key is None or value is None:
+            continue # so that this loop does never crash
+
+        if key.startswith("Nickname"):
+            info["nickname"] = value
+        elif key.startswith("Nationality"):
+            info["nationality"] = value
+        elif key.startswith("Height"):
+            pattern = "(?P<imperial>\d+.ft.\d+.in).\((?P<metric>[\d.]+.c?m)\)"
+            match = re.search(pattern, value.replace("\u00a0", " "))
+            if match is None:
+                info["height"] = None
+                continue
+            info["height"] = {
+                "metric": match.group("metric"),
+                "imperial": match.group("imperial")
+            }
+        elif key.startswith("Weight"):
+            pattern = ("(?P<imperial>\d+.lb).\((?P<metric>\d+.kg);.(?P<eng>["
+                       "\d.]+.st.(?:\d+.lb)?)")
+            match = re.search(pattern, value.replace("\u00a0", " "))
+            if match is None:
+                info["weight"] = None
+                continue
+            info["weight"] = {
+                "metric": match.group("metric"),
+                "imperial": match.group("imperial"),
+                "eng": match.group("eng")
+            }
+        elif key.startswith("Reach"):
+            pattern = "(?P<imperial>\d+.in).\((?P<metric>\d+.c?m)\)"
+            match = re.search(pattern, value.replace("\u00a0", " "))
+            if match is None:
+                info["reach"] = None
+                continue
+            info["reach"] = {
+                "metric": match.group("metric"),
+                "imperial": match.group("imperial")
+            }
     return info
+
+def get_ops_with_info(url):
+    ops = get_opponents(url)
+    for op in ops:
+        if link := op.get("url"):
+            info = get_info(link)
+        op["info"] = info
+    return ops
